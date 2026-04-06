30b17707d55c586557413a4c6af787601575fd38
Alex Eller <alex.eller88@gmail.com>
2026-04-06 17:30:17 -0400

    running dag with S3 access

--- dags/test_upload.py
+++ dags/test_upload.py
@@ -1,10 +1,32 @@
-from airflow.providers.amazon.aws.operators.s3 import S3CreateObjectOperator
+"""## S3 upload example DAG
 
-upload_file = S3CreateObjectOperator(
-    task_id="test_upload",
-    s3_bucket="astrolabs-634236767178-us-east-1-an",
-    s3_key="data/my_file.txt",
-    data="Hello from Astronomer!",
-    aws_conn_id="astro_s3_conn", # Match the ID from Step 1
+Creates/overwrites an object in S3 using the AWS provider's
+`S3CreateObjectOperator`.
+"""
+
+from airflow.sdk import dag
+from airflow.providers.amazon.aws.operators.s3 import S3CreateObjectOperator
+from pendulum import datetime
+
+
+@dag(
+    start_date=datetime(2025, 4, 22),
+    schedule=None,
+    catchup=False,
+    default_args={"owner": "Astro", "retries": 0},
+    tags=["example", "aws", "s3"],
+    doc_md=__doc__,
 )
+def test_upload():
+    S3CreateObjectOperator(
+        task_id="test_upload",
+        s3_bucket="astrolabs-634236767178-us-east-1-an",
+        s3_key="data/my_file.txt",
+        data="Hello from Astronomer!",
+        aws_conn_id="astro_s3_conn",  # Match the Connection ID in Airflow
+        replace=True,
+    )
+
+
+test_upload()
 
